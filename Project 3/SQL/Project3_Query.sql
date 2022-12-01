3cabf0c01cc6cc5e4fd70befdecc290aabf705d8
ashleyrabanales <arabana2@students.kennesaw.edu>
2022-12-01 16:49:48 -0500

    sql

---
+++ Project 3/SQL/Project3_Query.sql
@@ -0,0 +1,43 @@
+use Project3;
+-- Quesiton a--
+select p.ticker, p.analyst, a.Confidence_Score, a.Points, a.Error_rate, a.Accuracy_Percentile from predictions p left join analyst a on p.analyst = a.AnalystID
+where ticker = 'LULU'
+order by a.Confidence_Score desc;
+
+-- Quesiton b--
+
+select A1.industry, A1.companynumber, A2.analystnumber, (A2.analystnumber/A1.companynumber) AvgnumberAnalyst from
+(select industry, count(company) companynumber from companydata group by industry) A1
+left join
+(select count(p.analyst) analystnumber, c.industry from predictions p left join companydata c on p.ticker = c.ticker
+group by c.industry) A2 on A1.industry = A2.industry;
+
+select (avg(e_consensus) - avg(actual)) from eps_data
+where ticker in (select ticker from companydata
+where industry = 'Software');
+
+select c.industry, avg(e.e_consensus), avg(e.actual), (avg(e.e_consensus)- avg(e.actual)) bias from eps_data e left join companydata c on e.ticker = c.ticker
+group by c.industry;
+
+
+-- Question c--
+-- create a view of confidence score and disctint analyst per company in predictions
+CREATE OR REPLACE VIEW final1 as (
+select distinct(analyst), p.ticker, a.Confidence_Score
+from predictions p left join analyst a on p.analyst = a.AnalystID );
+
+select company, count(f.analyst)
+from companydata c join final1 f on c.ticker = f.ticker
+where f.Confidence_Score > 7
+group by company
+order by count(f.analyst) DESC;
+
+-- Question d--
+select company
+from companydata
+order by followers DESC
+limit 1;
+
+
+
+
